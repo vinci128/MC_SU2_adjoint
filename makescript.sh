a9890cb44a0e57e3b01d93f2330132b02c7fbc76
vinci128 <vinci128@gmail.com>
2019-10-30 15:31:22 +0100

    Optimization added in compiler

--- makescript.sh
+++ makescript.sh
@@ -4,15 +4,15 @@ mkdir O1minus_output_files
 mkdir O0plus_output_files
 mkdir log_files
 mkdir obs
-g++ -Wall -g -c ranlxd.c
-g++ -Wall -g -c geometry.cpp
-g++ -Wall -g -c archive.cpp
-g++ -Wall -g -c update.cpp
-g++ -Wall -g -c observables.cpp
-g++ -Wall -g -c APE_smearing.cpp
-g++ -Wall -g -c main_SU2_adjoint.cpp
-g++ -Wall -g -c measure.cpp
-g++ -Wall -g -c main_measure.cpp
-g++ -Wall -g ranlxd.o geometry.o update.o APE_smearing.o observables.o archive.o main_SU2_adjoint.o -o SU2_adjoint -lm
-g++ -Wall -g ranlxd.o geometry.o update.o APE_smearing.o observables.o archive.o measure.o -o measure -lm
-g++ -Wall -g ranlxd.o geometry.o update.o APE_smearing.o observables.o archive.o main_measure.o -o main_measure -lm
+g++ -O3 -Wall -g -c ranlxd.c
+g++ -O3 -Wall -g -c geometry.cpp
+g++ -O3 -Wall -g -c archive.cpp
+g++ -O3 -Wall -g -c update.cpp
+g++ -O3 -Wall -g -c observables.cpp
+g++ -O3 -Wall -g -c APE_smearing.cpp
+g++ -O3 -Wall -g -c main_SU2_adjoint.cpp
+g++ -O3 -Wall -g -c measure.cpp
+g++ -O3 -Wall -g -c main_measure.cpp
+g++ -O3 -Wall -g ranlxd.o geometry.o update.o APE_smearing.o observables.o archive.o main_SU2_adjoint.o -o SU2_adjoint -lm
+g++ -O3 -Wall -g ranlxd.o geometry.o update.o APE_smearing.o observables.o archive.o measure.o -o measure -lm
+g++ -O3 -Wall -g ranlxd.o geometry.o update.o APE_smearing.o observables.o archive.o main_measure.o -o main_measure -lm
